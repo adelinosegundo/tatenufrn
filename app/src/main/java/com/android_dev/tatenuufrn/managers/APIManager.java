3bc0d44c671aeecf8ff1402f65eec09140073cfc
Adelino Segundo <adelinosegundo@gmail.com>
2015-11-22 18:08:49 -0300

    fixed rate url

--- app/src/main/java/com/android_dev/tatenuufrn/managers/APIManager.java
+++ app/src/main/java/com/android_dev/tatenuufrn/managers/APIManager.java
@@ -1,6 +1,7 @@
 package com.android_dev.tatenuufrn.managers;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -9,6 +10,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.android_dev.tatenuufrn.applications.TatenuUFRNApplication;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
@@ -52,6 +54,7 @@ public class APIManager {
 
         queue.add(stringRequest);
     }
+
     public void logout(Context context, Response.Listener<String> responseListener){
         String url = apiServerUrl+apiPath+logoutPath;
         RequestQueue queue = Volley.newRequestQueue(context);
@@ -65,8 +68,15 @@ public class APIManager {
 
         queue.add(stringRequest);
     }
+
     public void events(Context context, Response.Listener<String> responseListener){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(TatenuUFRNApplication.SHARED_PREFERENCES_NAME, context.MODE_PRIVATE);
+        String lastUpdated = sharedPreferences.getString("lastUpdated", "");
+
         String url = apiServerUrl+apiPath+eventsPath;
+        if (!lastUpdated.equals(""))
+            url += "?last_updated=" + lastUpdated;
+
         RequestQueue queue = Volley.newRequestQueue(context);
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, responseListener, new Response.ErrorListener() {
@@ -78,6 +88,7 @@ public class APIManager {
 
         queue.add(stringRequest);
     }
+
     public void tellIAmGoing(Context context, Response.Listener<String> responseListener, String eventID){
         String url = apiServerUrl+apiPath+eventsPath+eventID+tellIAmGoingPath;
         RequestQueue queue = Volley.newRequestQueue(context);
@@ -104,8 +115,8 @@ public class APIManager {
 
         queue.add(stringRequest);
     }
-    public void rate(Context context, Response.Listener<String> responseListener, String eventID){
-        String url = apiServerUrl+apiPath+eventsPath+eventID+ratePath;
+    public void rate(Context context, Response.Listener<String> responseListener, String eventID, Float rate){
+        String url = apiServerUrl+apiPath+eventsPath+eventID+ratePath+"?rate="+String.valueOf(rate);
         RequestQueue queue = Volley.newRequestQueue(context);
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, responseListener, new Response.ErrorListener() {
