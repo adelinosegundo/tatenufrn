3dc95eecc056dc30a34706c04b5738e66723e4e7
Wendell P. Barreto <wendellp.barreto@gmail.com>
2015-11-22 14:52:34 -0300

    Add redesign.

--- app/src/main/java/com/android_dev/tatenuufrn/adapters/EventAdapter.java
+++ app/src/main/java/com/android_dev/tatenuufrn/adapters/EventAdapter.java
@@ -1,8 +1,6 @@
 package com.android_dev.tatenuufrn.adapters;
 
 import android.content.Context;
-import android.os.CountDownTimer;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,20 +13,21 @@ import com.android_dev.tatenuufrn.domain.Event;
 import com.android_dev.tatenuufrn.helpers.EventCountDownTimer;
 import com.raizlabs.android.dbflow.list.FlowQueryList;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by adelinosegundo on 10/8/15.
  */
 public class EventAdapter extends ArrayAdapter<Event> {
     private FlowQueryList<Event> events = new FlowQueryList<Event>(Event.class);
+
+    public EventAdapter(Context context, int id) {
+        super(context, id);
+        this.update();
+    }
 
     public void update() {
         this.clear();
@@ -39,69 +38,68 @@ public class EventAdapter extends ArrayAdapter<Event> {
         this.notifyDataSetChanged();
     }
 
-    static class ViewHolder{
-        TextView nameText;
-        TextView descriptionText;
-        TextView timeTitleText;
-        TextView timeText;
-        ImageView image;
-        EventCountDownTimer countDownTimer;
-    }
-
-    public EventAdapter(Context context, int id) {
-        super(context, id);
-        this.update();
-    }
-
     public View getView(int position, View view, ViewGroup parent) {
         ViewHolder holder = null;
+
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater)
                     getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.event_row, null);
+
             holder = new ViewHolder();
 
-            holder.nameText =  (TextView) view.findViewById(R.id.titleEventRowTextView);
-            //holder.descriptionText =  (TextView) view.findViewById(R.id.descriptionEventRowTextView);
-            holder.image = (ImageView) view.findViewById(R.id.eventRowImageView);
-            holder.timeTitleText = (TextView) view.findViewById(R.id.timeTitleEventRowTextView);
-            holder.timeText = (TextView) view.findViewById(R.id.timeEventRowTextView);
+            holder.title = (TextView) view.findViewById(R.id.titleEventRowTextView);
+            holder.image = (ImageView) view.findViewById(R.id.imageEventRowImageView);
+            holder.timeTitle = (TextView) view.findViewById(R.id.timeTitleEventRowTextView);
+            holder.time = (TextView) view.findViewById(R.id.timeEventRowTextView);
+
             view.setTag(holder);
         } else {
             holder = (ViewHolder) view.getTag();
         }
+
         Event event = getItem(position);
 
-        if (event!= null) {
-            Calendar nowCalendar = Calendar.getInstance();
-            nowCalendar.setTimeZone(TimeZone.getTimeZone("GMT"));
-            long nowMilis = System.currentTimeMillis();
-            nowMilis -= 10800000;
-
-            long startTime = event.getStartTime().longValue()*1000;
-            long endTime = event.getEndTime().longValue()*1000;
-
-            long timeToEvent = startTime - nowMilis;
-            long timeLeftOfEvent = endTime - nowMilis;
-
-            holder.nameText.setText(event.getTitle());
-            //holder.descriptionText.setText(event.getDescription());
+        if (event != null) {
+            holder.title.setText(event.getTitle());
             holder.image.setImageBitmap(event.getImageBitmap());
+
+//            Calendar nowCalendar = Calendar.getInstance();
+//            nowCalendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+            long nowMillis = System.currentTimeMillis();
+            nowMillis -= 10800000;
+
+            long startTimeMillis = event.getStartTime().longValue() * 1000;
+            long endTimeMillis = event.getEndTime().longValue() * 1000;
+
+            long timeToEventMillis = startTimeMillis - nowMillis;
+            long timeLeftOfEventMillis = endTimeMillis - nowMillis;
+
             if (holder.countDownTimer != null)
                 holder.countDownTimer.cancel();
-            if (timeToEvent > 0) {
-                holder.timeTitleText.setText("TIME TO EVENT");
-                holder.countDownTimer = new EventCountDownTimer(holder.timeText, timeToEvent, 1000, "Started");
+
+            if (timeToEventMillis > 0) {
+                holder.timeTitle.setText("TIME TO EVENT");
+                holder.countDownTimer = new EventCountDownTimer(holder.time, timeToEventMillis, 1000, "Started");
                 holder.countDownTimer.start();
-            } else if (timeLeftOfEvent > 0) {
-                holder.timeTitleText.setText("TIME LEFT");
-                holder.countDownTimer = new EventCountDownTimer(holder.timeText, timeLeftOfEvent, 1000, "Over");
+            } else if (timeLeftOfEventMillis > 0) {
+                holder.timeTitle.setText("TIME LEFT");
+                holder.countDownTimer = new EventCountDownTimer(holder.time, timeLeftOfEventMillis, 1000, "Over");
                 holder.countDownTimer.start();
             } else {
-                holder.timeTitleText.setText("DATE");
-                holder.timeText.setText("15/10");
+                holder.timeTitle.setText("");
+                holder.time.setText("");
             }
         }
         return view;
+    }
+
+    static class ViewHolder {
+        TextView title;
+        TextView timeTitle;
+        TextView time;
+        ImageView image;
+        EventCountDownTimer countDownTimer;
     }
 }
--- app/src/main/java/com/android_dev/tatenuufrn/helpers/EventCountDownTimer.java
+++ app/src/main/java/com/android_dev/tatenuufrn/helpers/EventCountDownTimer.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
  * Created by adelinosegundo on 10/14/15.
  */
 public class EventCountDownTimer extends CountDownTimer {
-
     private TextView timeTextView;
     private String finishMessage;
 
@@ -17,33 +16,26 @@ public class EventCountDownTimer extends CountDownTimer {
         super(millisInFuture, countDownInterval);
         this.timeTextView = timeTexView;
         this.finishMessage = finishMessage;
+    }
 
+    private static String millisToCountDownTimeString(long millis) {
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        millis -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        String countDownTimeString = (days > 0) ?
+                days + "D " + hours + ":" + minutes + ":" + seconds : hours + ":" + minutes + ":" + seconds;
+
+        return countDownTimeString;
     }
 
     @Override
     public void onTick(long millisUntilFinished) {
-        this.timeTextView.setText(EventCountDownTimer.getTimeBetweenDates(millisUntilFinished));
-    }
-
-    private static String getTimeBetweenDates(long miliesLeft) {
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(miliesLeft);
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(miliesLeft);
-        long hours = TimeUnit.MILLISECONDS.toHours(miliesLeft);
-        long days = TimeUnit.MILLISECONDS.toDays(miliesLeft);
-
-        minutes -= hours*60;
-
-        String timeBetweenDates = "";
-        if (days > 0){
-            timeBetweenDates += days + " days ";
-            seconds -= days * 24 * 60 * 60;
-        }
-        timeBetweenDates += hours + ":";
-        seconds -= hours * 60 * 60;
-        timeBetweenDates += minutes + ":";
-        seconds -= minutes * 60;
-        timeBetweenDates += seconds;
-        return timeBetweenDates;
+        this.timeTextView.setText(EventCountDownTimer.millisToCountDownTimeString(millisUntilFinished));
     }
 
     @Override
