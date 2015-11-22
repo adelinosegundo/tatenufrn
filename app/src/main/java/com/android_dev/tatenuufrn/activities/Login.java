fe05569d23560b1fcb288e4d8dd22fe98a7ee9b6
Adelino Segundo <adelinosegundo@gmail.com>
2015-11-22 12:17:53 -0300

    sigaa api

--- app/src/main/java/com/android_dev/tatenuufrn/activities/Login.java
+++ app/src/main/java/com/android_dev/tatenuufrn/activities/Login.java
@@ -1,63 +1,109 @@
 package com.android_dev.tatenuufrn.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android_dev.tatenuufrn.R;
 import com.android_dev.tatenuufrn.applications.TatenuUFRNApplication;
-import com.android_dev.tatenuufrn.domain.Event;
-import com.android_dev.tatenuufrn.helpers.DateHelper;
-import com.raizlabs.android.dbflow.structure.container.JSONModel;
+import com.android_dev.tatenuufrn.vendor.OAuthTokenRequest;
+import com.google.api.client.auth.oauth2.Credential;
+import com.wuman.android.auth.OAuthManager;
 
-import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 
 public class Login extends Activity {
-    private EditText usernameEditText;
-    private EditText passowrdEditText;
     private Button loginButton;
-    private Button populateButton;
+    private String jsonResponse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         SharedPreferences sharedPreferences = getSharedPreferences(TatenuUFRNApplication.SHARED_PREFERENCES_NAME, MODE_PRIVATE);
-
-
-        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
-        passowrdEditText = (EditText) findViewById(R.id.passwordEditText);
-
-        usernameEditText.setText(sharedPreferences.getString("username", ""));
-        passowrdEditText.setText(sharedPreferences.getString("password", ""));
+        sharedPreferences.getString("userToken", "");
 
         loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences(TatenuUFRNApplication.SHARED_PREFERENCES_NAME, MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("username", usernameEditText.getText().toString());
-                editor.putString("password", passowrdEditText.getText().toString());
-                editor.commit();
-                Intent listEventsIntent = new Intent(getBaseContext(), ListEvents.class);
-                startActivity(listEventsIntent);
+                login();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        Log.d("Result", "reqCode: " + String.valueOf(requestCode) + " resCode: " + String.valueOf(resultCode));
+    }
+
+    public void login(){
+        OAuthTokenRequest.getInstance().getTokenCredential(this, "http://apitestes.info.ufrn.br/authz-server", "taten-ufrn-id", "tatenufrn", new OAuthManager.OAuthCallback<Credential>() {
+            @Override public void run(OAuthManager.OAuthFuture<Credential> future) {
+                try {
+                    Credential credential = future.getResult();
+                    OAuthTokenRequest.getInstance().setCredentials(credential);
+                    if (credential != null) {
+                        getUserData();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // make API queries with credential.getAccessToken()
+            }
+        });
+    }
+
+    public void getUserData(){
+        final Context context = this;
+
+        String urlJsonObj = "http://apitestes.info.ufrn.br/usuario-services/services/usuario/info";
+
+        OAuthTokenRequest.getInstance().resourceRequest(this, Request.Method.GET, urlJsonObj, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    String nome = jsonObject.getString("nome");
+                    String login = jsonObject.getString("login");
+
+                    jsonResponse = "";
+                    jsonResponse += "Name: " + nome + "\n\n";
+                    jsonResponse += "Login: " + login + "\n\n";
+
+                    VolleyLog.d("SAID", "UserData", response);
+                    Log.i("USERDATA", response);
+                    Intent intent = new Intent(context, ListEvents.class);
+                    context.startActivity(intent);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("SAIDA", "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
